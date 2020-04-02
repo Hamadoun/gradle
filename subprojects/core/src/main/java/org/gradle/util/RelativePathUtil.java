4076c46002fa3a562ac3f358bbcac14fadd9afef
Cedric Champeau <cedric@gradle.com>
2020-04-02 16:07:41 +0200

    WIP

--- subprojects/core/src/main/java/org/gradle/util/RelativePathUtil.java
+++ subprojects/core/src/main/java/org/gradle/util/RelativePathUtil.java
@@ -19,6 +19,10 @@ package org.gradle.util;
 import org.gradle.internal.UncheckedException;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class RelativePathUtil {
     /**
@@ -30,9 +34,82 @@ public class RelativePathUtil {
      */
     public static String relativePath(File from, File to) {
         try {
-            return TextUtil.normaliseFileSeparators(from.toPath().relativize(to.toPath()).toString());
+            return getRelativePath(from, to);
         } catch (Exception e) {
             throw UncheckedException.throwAsUncheckedException(e);
         }
+    }
+
+    // The following methods are _copied_ from Ant FileUtils, in order not to depend on an external library
+    // for this
+
+    private static String getRelativePath(File fromFile, File toFile) throws Exception { //NOSONAR
+        String fromPath = fromFile.getCanonicalPath();
+        String toPath = toFile.getCanonicalPath();
+
+        // build the path stack info to compare
+        String[] fromPathStack = getPathStack(fromPath);
+        String[] toPathStack = getPathStack(toPath);
+
+        if (0 < toPathStack.length && 0 < fromPathStack.length) {
+            if (!fromPathStack[0].equals(toPathStack[0])) {
+                // not the same device (would be "" on Linux/Unix)
+
+                return getPath(Arrays.asList(toPathStack));
+            }
+        } else {
+            // no comparison possible
+            return getPath(Arrays.asList(toPathStack));
+        }
+
+        int minLength = Math.min(fromPathStack.length, toPathStack.length);
+        int same = 1; // Used outside the for loop
+
+        // get index of parts which are equal
+        for (;
+             same < minLength && fromPathStack[same].equals(toPathStack[same]);
+             same++) {
+            // Do nothing
+        }
+
+        List relativePathStack = new ArrayList();
+
+        // if "from" part is longer, fill it up with ".."
+        // to reach path which is equal to both paths
+        for (int i = same; i < fromPathStack.length; i++) {
+            relativePathStack.add("..");
+        }
+
+        // fill it up path with parts which were not equal
+        for (int i = same; i < toPathStack.length; i++) {
+            relativePathStack.add(toPathStack[i]);
+        }
+
+        return getPath(relativePathStack);
+    }
+
+    private static String[] getPathStack(String path) {
+        String normalizedPath = path.replace(File.separatorChar, '/');
+
+        return normalizedPath.split("/");
+    }
+
+    public static String getPath(List pathStack) {
+        // can safely use '/' because Windows understands '/' as separator
+        return getPath(pathStack, '/');
+    }
+    
+    private static String getPath(final List pathStack, final char separatorChar) {
+        final StringBuffer buffer = new StringBuffer();
+
+        final Iterator iter = pathStack.iterator();
+        if (iter.hasNext()) {
+            buffer.append(iter.next());
+        }
+        while (iter.hasNext()) {
+            buffer.append(separatorChar);
+            buffer.append(iter.next());
+        }
+        return buffer.toString();
     }
 }
